21184810c88ddd6bbb5d5ddf3fc68c7c6db1a523
Christopher Artis <mrchrisomat@gmail.com>
2025-05-18 13:21:46 -0400

    App.js and MovieCard.jsx early implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import MovieCard from './MovieCard';
 import './App.css';
 
+const API_URL = 'https://omdbapi.com?apikey=fe2f6c44'
+
 function App() {
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState([])
+  const searchMovies = async (title) => {
+    await fetch(`${API_URL}&s=${title}`).then(res => {
+      setMovies(res.json().data.Search)
+    })
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
 }
 
